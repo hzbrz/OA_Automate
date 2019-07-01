cfa2e8e26f2a56a68d50c64a6c472b6664178ec7
Hasan Zahid <hasanthedev@gmail.com>
2019-06-30 22:15:15 -0500

    fixed program break when page detail won't load, starting navigation process, changed loop into indices

--- walmart_deal_scraper.py
+++ walmart_deal_scraper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from secrets import dynamic_link
@@ -19,9 +20,9 @@ ul = product_div.find_element_by_class_name("search-result-gridview-items")
 # getting all list items that contain each item
 li_items = ul.find_elements_by_class_name("Grid-col")
 
-for li in li_items:
+for i in range(len(li_items)):
   # getting inside the list item so I can get more data for each item
-  li_inner = li.find_element_by_class_name("search-result-gridview-item")
+  li_inner = li_items[i].find_element_by_class_name("search-result-gridview-item")
 
   # getting the name 
   product_name = li_inner.find_element_by_class_name("product-title-link").get_attribute("title")
@@ -33,7 +34,6 @@ for li in li_items:
   item_price = li_inner.find_element_by_class_name("price-group").get_attribute("aria-label")
 
   # # TWO WAYS TO DO THE SAME THING:
-
   # ## 1: I get the company name in this method by opening a new window which caused the connection to temporarily timeout
   # # creating another driver to bypass a stale element exception/issue
   # driver_two = webdriver.Chrome()
@@ -45,6 +45,7 @@ for li in li_items:
   # # navigating back to the page with all other products and repeating process
   # driver_two.close()
 
+  # ------------------------- TODO: generateTab func ---------------------------------------
   ## 2: I get the company name by opening a new tab in the same window, process is faster
   # opens a new empty tab
   driver.execute_script("window.open('');")
@@ -53,20 +54,32 @@ for li in li_items:
   driver.switch_to.window(driver.window_handles[1])
   # navigate to the prouct details page
   driver.get(product_link)
+  time.sleep(.5)
   # locating the area of the page for the company name for the product and getting the company name
-  company_name = driver.find_element_by_class_name("prod-brandName").get_attribute('text')
+  try:
+    # the try-atch is due to walmart sometimes not being able to load the product details page
+    company_name = driver.find_element_by_class_name("prod-brandName").get_attribute('text')
+  except:
+    print("company name page bugged out")
+    company_name = "couldn't find"
   # close the active tab
   driver.close()
   # Switch the focus back to the first tab
   driver.switch_to.window(driver.window_handles[0])
   time.sleep(.5)
-
+  #---------------------------------------------------------------------------------------
   print(product_name, "\n", item_price, "\n", product_link, "\n", company_name, "\n------------")
+  print(i, len(li_items))
+  # hook to see if all items have been scraped on the page and use it to navigate to next page
+  if i+1 == len(li_items):
+    print("page 2")
+
 
 
 # TODO 1: figure out navigation between pages by changing the (dynamic_link) imported from secrets - hz 07/01
 # TODO 2: switch to another category when there is no more products to scrape in one category - hz 07/02
 # TODO 3: figure out optimal way to modify the link and automate moving categories -hz
+# TODO 4: Put all product data in a dictionary, the key should be number or name?
 # TODO 4: turn code into componenets through functions (GENERAL GOAL)
 
 driver.close()
